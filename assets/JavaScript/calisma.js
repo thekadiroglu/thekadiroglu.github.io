eeae0b0157b490f4bcabf3d08710f8ffee85bbc5
emirhan <rza.emirhan@gmail.com>
2024-02-03 21:48:28 +0300

    v3.0
    congrats!! out on v3.0

--- assets/JavaScript/calisma.js
+++ assets/JavaScript/calisma.js
@@ -5,11 +5,6 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
-window.addEventListener('scroll', function() {
-  var header = document.querySelector('header');
-  var scrolled = window.scrollY > 0;
-  header.classList.toggle('scrolled', scrolled);
-});
 // Modalı göster
 function modalGoster() {
   var modal = document.getElementById('modal');
@@ -38,15 +33,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const body = document.body;
   const navbarLinks = document.querySelectorAll('.navbar a');
   const baslik = document.querySelector(".baslik");
-  const cardTitles = document.querySelectorAll('.cards .card h1');
-  const menuIcon = document.querySelector('.menu-icon');  
-  const darkMode = document.querySelector('.darkMode i');
+  const cardTitles = document.querySelectorAll('.cards .card h1');  
+  const header = document.querySelector('header'); // Yeni eklenen satır
 
   darkModeToggle.addEventListener('click', function () {
     body.classList.toggle('dark-mode');
-    baslik.classList.toggle('dark-text');
-    menuIcon.classList.toggle('dark-text');
-    darkMode.classList.toggle('dark-text'); 
+    baslik.classList.toggle('dark-text'); 
+    header.style.background = body.classList.contains('dark-mode') ? '#222222' : '';
     navbarLinks.forEach(function (link) {
       link.classList.toggle('dark-text');
     });
@@ -56,3 +49,4 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
